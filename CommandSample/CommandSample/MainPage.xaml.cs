f9b92de226939143dad6b491aaaebc445108b42e
Will Beal <willbeal@outlook.com>
2020-03-30 16:11:29 +0200

    Add project files.

---
+++ CommandSample/CommandSample/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CommandSample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        private MainPageViewModel viewModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            viewModel = (MainPageViewModel)BindingContext;
+        }
+    }
+}
---
+++ CommandSample/CommandSample/MainPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace CommandSample
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel() => AddCommand = new Xamarin.Forms.Command(OnAddCommandExecuted);
+
+        public ICommand AddCommand { get; }
+
+        private int number;
+
+        public int Number
+        {
+            get => number;
+            set
+            {
+                if (value != number)
+                {
+                    number = value;
+                    NotifyPropertyChanged(nameof(Number));
+                }
+            }
+        }
+
+        private void OnAddCommandExecuted() => Number++;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
